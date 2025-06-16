dedf8a99ad8384ba7a96359b3ce4bf9d6a842828
triston <tristonwhite21@gmail.com>
2025-06-16 11:05:50 -0500

    finished midterm

---
+++ sophomore-year/spring2025/scripting-class/Assignment 5 Midterm/assignment5.py
@@ -0,0 +1,37 @@
+"""
+For this assignment type out a program that when it runs it will be a Mad Lib generator and selector. 
+You will have a program whose main body is a menu, asking the user to select one of 3 available Mad Libs to play. Once the user selects one, the program goes to the appropriate FUNCTION for that Mad Lib, where it will ask the needed questions and produce the story. Upon return to the body after the Mad Lib has been shown, the program will ask if the user wants to go again and repeats these steps until the user says no. 
+
+When returning to the main menu make sure the program tells the user some form of, â€œBack to main menuâ€
+
+1.	
+The first Mad Lib is called Invasion. You will need to ask the user for 9 things: a noun, a city, a number, a period of time, adjective 1, adjective 2, a person's name, a type of clothing and a color. The trick to this one is that is uses some of the user entered answers multiple times. Pay attention to the numbers. 
+
+"It is a little known fact that (noun 1)s have been watching (city 1) for (number 1) (period of time 1). Only the (adjective 1) people believed that it was just a matter of time before (noun 1)s invaded the (adjective 2) city of (city 1). (person's name 1), who was a (adjective 1) person tried to warn the people, telling them their best defense was to hang (type of clothing 1)s from the trees and (color 1) shoes on every door knob. Unfortunately no one believed (person's name 1) and when (noun 1)s attacked (city 1), only the (adjective 1) people survived."
+
+2.	
+The second Mad Lib is called Romeo and Juliet. You will need to ask the user for 11 things. 2 different noun plurals, a place, 2 different nouns, 2 different adjectives, a number, 2 different verbs and a body part. This trick to this one is that it needs to display to the user as shown below with the sentences split and each line starting with a capital. 
+
+Two (noun plural), both alike in dignity,
+In fair (place), where we lay our scene,
+From ancient (noun) break to new mutiny,
+Where civil blood makes civil hands unclean.
+From forth the fatal loins of these two foes
+A pair of star-cross`d (noun plural) take their life;
+Whole misadventured piteous overthrows
+Do with their (noun) bury their parents` strife.
+The fearful passage of their (adjective) love,
+And the continuance of their parents` rage,
+Which, but their children`s end, nought could (verb),
+Is now the (number) hours` traffic of our stage;
+The which if you with (adjective) (body part) attend,
+What here shall (verb), our toil shall strive to mend.
+
+
+
+
+3.	
+The third is called Walmart. You will need to ask the user for 15 things. 2 verbs, a verb ending with â€œingâ€, 5 different adjectives, 5 different noun plurals, and 1 relative(family member) plural. No specific trick to this one. Just have to keep track of everything. 
+
+Come (Verb) at WALMART, where you`ll receive (Adjective) discounts on all of your favorite brand name (Noun Plural). Our (Adjective) and (Verb Ending In "Ing") associates are there to (Verb) you (Number) hours a day. Here you will find (Adjective) prices on the (Noun Plural) you need. (Noun Plural) for the moms, (Noun Plural) for the kids and all the latest electronics for the (Relative Plural). So come on down to your (Adjective) (Adjective) WALMART where the (Noun Plural) come first.
+"""
